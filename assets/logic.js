867dc9d117730bc4067244cacc2d8035c2a173a8
Lakshdeep Bajwa <bajwals@live.com>
2019-11-03 00:00:47 -0700

    users into array

--- assets/logic.js
+++ assets/logic.js
@@ -1,8 +1,42 @@
-function createColorPicker() {
+var users = [
+    {
+        "username": "bobcat",
+        "message": "",
+    },
+    {
+        "username": "gorilla",
+        "message": "",
+    },
+    {
+        "username": "rabbit",
+        "message": "",
+    },
+    {
+        "username": "yak",
+        "message": "",
+    },]
+var userChoice;
 
-};
 
-function pickColor() {
+$("#bobcat").on("click", function() {
+    userChoice = users[0].username;
+    $("#button-holder").attr("display", "none");
+    console.log(userChoice);
+})
+$("#gorilla").on("click", function() {
+    userChoice = users[1].username;
+    $("#button-holder").attr("display", "none");
+    console.log(userChoice);
+})
+$("#rabbit").on("click", function() {
+    userChoice = users[2].username;
+    $("#button-holder").attr("display", "none");
+    console.log(userChoice);
+})
+$("#yak").on("click", function() {
+    userChoice = users[3].username;
+    $("#button-holder").attr("display", "none");
+    console.log(userChoice);
+})
 
-};
 
